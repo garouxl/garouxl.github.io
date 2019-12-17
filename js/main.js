12c7d85feca48d78a10c5501724b736f52596b78
Leandro Almeida <garou_xl@yahoo.com.br>
2019-12-17 12:27:25 -0300

    Alterado o JS para as barras, HTML e CSS

--- js/main.js
+++ js/main.js
@@ -1,49 +1,47 @@
-((win, doc) => {
-    "use strict";
-    // seta vertical
-    function resizeBody() {
-        doc.getElementsByTagName("body")[0].style.height =
-            window.innerHeight + "px";
+;((win, doc) => {
+  'use strict'
+  const $body = doc.querySelector('body')
+  const $main = doc.querySelector('main')
+  const $targetV = doc.querySelector('.targetV')
+  const $targetH = doc.querySelector('.targetH')
+  const $doge = doc.querySelector('.doge')
+  // seta vertical
+  function resizeBody () {
+    $body.style.height =
+      window.innerHeight + 'px'
+  }
+  // barras de target
+  function target (event) {
+      console.log(event)
+    $targetV.style.transform = `translateX(${event.layerX}px)`
+    $targetH.style.transform = `translateY(${event.layerY}px)`
+  }
+  // detrava a tela
+  function unLock () {
+    const barsL = Array.from(doc.querySelectorAll('.entryBar:nth-child(odd)'))
+    const barsR = Array.from(doc.querySelectorAll('.entryBar:nth-child(even)'))
+    for (let i = 0; i <= barsL.length - 1; i++) {
+      barsL[i].classList.add('entryBarL')
+      barsR[i].classList.add('entryBarR')
     }
-    // barras de target
-    function target(event) {
-        doc.getElementsByClassName(
-            "targetV"
-        )[0].style.transform = `translateX(${event.offsetX}px)`;
-        doc.getElementsByClassName(
-            "targetH"
-        )[0].style.transform = `translateY(${event.offsetY}px)`;
-    }
-    // detrava a tela
-    function unLock() {
-        let barsL = Array.from(
-            doc.querySelectorAll(".entryBar:nth-child(odd)")
-        );
-        let barsR = Array.from(
-            doc.querySelectorAll(".entryBar:nth-child(even)")
-        );
-        for (let i = 0; i <= barsL.length - 1; i++) {
-            barsL[i].classList.add("entryBarL");
-            barsR[i].classList.add("entryBarR");
-        }
-        doc.getElementById("main").addEventListener("click", target);
-    }
+    $main.addEventListener('click', target, false)
+  }
 
-    function konami(callback) {
-        let keys = [],
-            konami = "38,38,40,40,37,39,37,39,66,65";
-        doc.onkeydown = function(e) {
-            keys.push(e.keyCode);
-            if (keys.toString().indexOf(konami) >= 0) {
-                callback();
-                keys = [];
-            }
-        };
+  function konami (callback) {
+    let keys = []
+    const konami = '38,38,40,40,37,39,37,39,66,65'
+    doc.onkeydown = function (e) {
+      keys.push(e.keyCode)
+      if (keys.toString().indexOf(konami) >= 0) {
+        callback()
+        keys = []
+      }
     }
-    resizeBody();
-    win.addEventListener("resize", resizeBody);
-    doc.getElementById("main").addEventListener("click", unLock);
-    konami(() =>
-        doc.getElementsByClassName("doge")[0].classList.add("doge-animate")
-    );
-})(window, document);
+  }
+  resizeBody()
+  win.addEventListener('resize', resizeBody)
+  $main.addEventListener('click', unLock)
+  konami(() =>
+    $doge.classList.toggle('doge-animate')
+  )
+})(window, document)
