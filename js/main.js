fe4aa0850d793deb6abec06e8a770b24d97bf88a
Leandro Almeida <garou_xl@yahoo.com.br>
2020-02-14 14:16:45 -0300

    Added new project and tweaked animation

--- js/main.js
+++ js/main.js
@@ -2,20 +2,21 @@
   'use strict'
   const $body = doc.querySelector('body')
   const $main = doc.querySelector('main')
-  const $targetV = doc.querySelector('.target-v')
-  const $targetH = doc.querySelector('.target-h')
+/*   const $targetV = doc.querySelector('.target-v')
+  const $targetH = doc.querySelector('.target-h') */
   const $doge = doc.querySelector('.doge')
-  const $link = doc.querySelectorAll('.list__link')
+  const $link = doc.querySelectorAll('[data-js=link]')
+  const $easter = doc.querySelector('[data-js=easter]')
   // seta vertical
   function resizeBody () {
     $body.style.height =
       window.innerHeight + 'px'
   }
   // barras de target
-  function target (event) {
+/*   function target (event) {
     $targetV.style.transform = `translateX(${event.layerX}px)`
     $targetH.style.transform = `translateY(${event.layerY}px)`
-  }
+  } */
   // destrava a tela
   function unLock () {
     const barsL = Array.from(doc.querySelectorAll('.entry__bar:nth-child(odd)'))
@@ -24,7 +25,7 @@
       barsL[i].classList.add('entry__bar--left')
       barsR[i].classList.add('entry__bar--right')
     }
-    $main.addEventListener('click', target, false)
+    // $main.addEventListener('click', target, false)
   }
 
   function konami (callback) {
@@ -34,6 +35,7 @@
       keys.push(e.keyCode)
       if (keys.toString().indexOf(konami) >= 0) {
         callback()
+        $easter.remove()
         keys = []
       }
     }
