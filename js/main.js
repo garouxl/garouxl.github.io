63bcc137d0c256e8b894267535180430a8346174
Leandro Almeida <vempari@gmail.com>
2021-09-14 16:06:14 -0300

    add tilt

--- js/main.js
+++ js/main.js
@@ -77,6 +77,22 @@
         $doge.classList.toggle('doge-animate')
       )
       olar()
+
+      VanillaTilt.init(document.querySelector(".main"), {
+        max: 5,     // max tilt rotation (degrees)
+        startX: 0,      // the starting tilt on the X axis, in degrees.
+        startY: 0,      // the starting tilt on the Y axis, in degrees.
+        perspective: 2000,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale: 1,      // 2 = 200%, 1.5 = 150%, etc..
+        speed: 300,    // Speed of the enter/exit transition
+        transition: true,   // Set a transition on enter/exit.
+        axis: null,   // What axis should be disabled. Can be X or Y.
+        reset: true,    // If the tilt effect has to be reset on exit.
+        easing: 'cubic-bezier(0, 0, 0.2, 1)',    // Easing on enter/exit.
+        glare: true,   // if it should have a "glare" effect
+        "max-glare": 0.6,
+        reverse: true,
+      })
     }
 
     return {
